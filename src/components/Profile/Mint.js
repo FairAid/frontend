667554742eeaa29a5eccf7b6c8c1801ec1c9e457
hara-desu <vladawinter33@gmail.com>
2024-08-06 15:29:18 +0900

    Commented out Mint.js

--- src/components/Profile/Mint.js
+++ src/components/Profile/Mint.js
@@ -5,49 +5,49 @@ import { toHex, utf8ToBytes, bytesToUtf8 } from 'ethereum-cryptography/utils';
 import { ethers } from 'ethers';
 
 const Mint = () => {
-    const { signer } = useAuth();
-    const publicKey = `${signer.address}`;
-    const privateKey = 'random private key';
-    console.log(signer.address); // MetaMask address is the public key
+    // const { signer } = useAuth();
+    // const publicKey = `${signer.address}`;
+    // const privateKey = 'random private key';
+    // console.log(signer.address); // MetaMask address is the public key
 
-    const data = {
-        "name": "Example NFT",
-        "description": "This is an example NFT",
-        "image": "ipfs://Qm... (link to image)",
-        "attributes": [
-          {
-            "trait_type": "Background",
-            "value": "Blue"
-          },
-          {
-            "trait_type": "Rarity",
-            "value": "Rare"
-          }
-        ]
-      }
+    // const data = {
+    //     "name": "Example NFT",
+    //     "description": "This is an example NFT",
+    //     "image": "ipfs://Qm... (link to image)",
+    //     "attributes": [
+    //       {
+    //         "trait_type": "Background",
+    //         "value": "Blue"
+    //       },
+    //       {
+    //         "trait_type": "Rarity",
+    //         "value": "Rare"
+    //       }
+    //     ]
+    //   }
 
-    const [encryptedMessage, setEncryptedMessage] = useState('');
-    const [decryptedMessage, setDecryptedMessage] = useState('');
+    // const [encryptedMessage, setEncryptedMessage] = useState('');
+    // const [decryptedMessage, setDecryptedMessage] = useState('');
   
-    const encryptWithPublicKey = (data, publicKey) => {
-        const pubKey = ethers.utils.arrayify(publicKey);
-        const messageBytes = utf8ToBytes(data);
-        const encrypted = encrypt(pubKey, messageBytes);
-        return toHex(encrypted);
-    };
-    const encrypted = encryptWithPublicKey(data, publicKey);
-    setEncryptedMessage(encrypted);
-    console.log(encryptedMessage);
+    // const encryptWithPublicKey = (data, publicKey) => {
+    //     const pubKey = ethers.utils.arrayify(publicKey);
+    //     const messageBytes = utf8ToBytes(data);
+    //     const encrypted = encrypt(pubKey, messageBytes);
+    //     return toHex(encrypted);
+    // };
+    // const encrypted = encryptWithPublicKey(data, publicKey);
+    // setEncryptedMessage(encrypted);
+    // console.log(encryptedMessage);
   
-    const decryptWithPrivateKey = (encryptedData, privateKey) => {
-        const privKey = ethers.utils.arrayify(privateKey);
-        const encryptedBytes = ethers.utils.arrayify(encryptedData);
-        const decrypted = decrypt(privKey, encryptedBytes);
-        return bytesToUtf8(decrypted);
-    };
-    const decrypted = decryptWithPrivateKey(encryptedMessage, privateKey);
-    setDecryptedMessage(decrypted);
-    console.log(decryptedMessage);
+    // const decryptWithPrivateKey = (encryptedData, privateKey) => {
+    //     const privKey = ethers.utils.arrayify(privateKey);
+    //     const encryptedBytes = ethers.utils.arrayify(encryptedData);
+    //     const decrypted = decrypt(privKey, encryptedBytes);
+    //     return bytesToUtf8(decrypted);
+    // };
+    // const decrypted = decryptWithPrivateKey(encryptedMessage, privateKey);
+    // setDecryptedMessage(decrypted);
+    // console.log(decryptedMessage);
 
     return (
         <>
